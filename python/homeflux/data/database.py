e0d1d46d68ffca8375053f280a972af27182b74d
david2777 <daduvo11@gmail.com>
2021-08-16 20:46:40 -0700

    + Add timer = Update to use context manager

--- python/homeflux/data/database.py
+++ python/homeflux/data/database.py
@@ -4,6 +4,7 @@ from typing import List
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+from homeflux.utils import timer
 from homeflux import environment, log
 from homeflux.data import data_types
 
@@ -18,22 +19,21 @@ def write(values: List[data_types.AbstractRecord]) -> None:
         None
     """
     # Open connection to database
+    t = timer.Timer()
     client = InfluxDBClient(url=environment.INFLUX_URL, token=environment.INFLUX_TOKEN, org=environment.INFLUX_ORG)
     log.debug('Writing %s points to %s', len(values), environment.INFLUX_URL)
-    api = client.write_api(write_options=SYNCHRONOUS)
+    with client.write_api(write_options=SYNCHRONOUS) as api:
+        # Group items by bucket
+        out_dict = {}
+        for obj in values:
+            if obj.bucket not in out_dict:
+                out_dict[obj.bucket] = []
+            out_dict[obj.bucket].append(obj.as_influx_dict())
 
-    # Group items by bucket
-    out_dict = {}
-    for obj in values:
-        if obj.bucket not in out_dict:
-            out_dict[obj.bucket] = []
-        out_dict[obj.bucket].append(obj.as_influx_dict())
+        # Insert data into database
+        for bucket, data_list in out_dict.items():
+            log.info('Writing %s points to bucket: %s', len(data_list), bucket)
+            for data_to_write in data_list:
+                api.write(bucket, environment.INFLUX_ORG, data_to_write)
 
-    # Insert data into database
-    for bucket, data_list in out_dict.items():
-        log.info('Writing %s points to bucket: %s', len(data_list), bucket)
-        for data_to_write in data_list:
-            api.write(bucket, environment.INFLUX_ORG, data_to_write)
-
-    # Close the connection
-    client.close()
+    log.info('Took %s seconds to write %s points %s', t.end(), len(values), environment.INFLUX_URL)
