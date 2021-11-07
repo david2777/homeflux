2fcfa79abfeafbf9b64b723eed3b821a24b53a44
david2777 <daduvo11@gmail.com>
2021-11-06 20:52:28 -0700

    + Add test_nut.py

---
+++ python/homeflux/_tests/agents/test_nut.py
@@ -0,0 +1,16 @@
+import unittest
+
+from homeflux.agents import nut
+
+
+class TestNut(unittest.TestCase):
+    def test_init(self):
+        ins = nut.NutClient('localhost', '127.0.0.1', 'hour')
+        self.assertIsInstance(ins, nut.NutClient)
+
+    def test_bad_timescale_init(self):
+        self.assertRaises(ValueError, nut.NutClient('localhost', '127.0.0.1', 'year'))
+
+
+if __name__ == '__main__':
+    unittest.main()
