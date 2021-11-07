4f7c4b855bd69b0b132d480cac133582445cacf5
david2777 <daduvo11@gmail.com>
2021-11-06 20:54:11 -0700

    ~ Fix test_bad_timescale_init

--- python/homeflux/_tests/agents/test_nut.py
+++ python/homeflux/_tests/agents/test_nut.py
@@ -9,7 +9,8 @@ class TestNut(unittest.TestCase):
         self.assertIsInstance(ins, nut.NutClient)
 
     def test_bad_timescale_init(self):
-        self.assertRaises(ValueError, nut.NutClient('localhost', '127.0.0.1', 'year'))
+        with self.assertRaises(ValueError):
+            nut.NutClient('localhost', '127.0.0.1', 'year')
 
 
 if __name__ == '__main__':
